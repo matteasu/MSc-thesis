bed0c594b65613697abf9e51164bfc71e2d316d8
Matteo Asuni <asunimatteo@gmail.com>
2023-12-02 19:15:31 +0100

    get functions

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 import os
 import re
+import json
 from utils import utils
-names = ["Choice","Player","RandomPlayer","HumanPlayer","BeatLastPlayer","BeatSelfPlayer","Game","main"]
-stdLib = ["\|file:///usr/include/c++/8\|",
-"\|file:///usr/include/x86_64-linux-gnu/c++/8\|",
-"\|file:///usr/include/c++/8/backward\|",
-"\|file:///usr/lib/gcc/x86_64-linux-gnu/8/include\|",
-"\|file:///usr/local/include\|",
-"\|file:///usr/lib/gcc/x86_64-linux-gnu/8/include-fixed\|",
-"\|file:///usr/include\|"]
-path="\|file:///home/masuni/rps-cpp/\|"
+from parsers.cpp import Cpp
+
+names = [
+    "Choice",
+    "Player",
+    "RandomPlayer",
+    "HumanPlayer",
+    "BeatLastPlayer",
+    "BeatSelfPlayer",
+    "Game",
+    "main",
+]
+stdLib = [
+    "\|file:///usr/include/c++/8\|",
+    "\|file:///usr/include/x86_64-linux-gnu/c++/8\|",
+    "\|file:///usr/include/c++/8/backward\|",
+    "\|file:///usr/lib/gcc/x86_64-linux-gnu/8/include\|",
+    "\|file:///usr/local/include\|",
+    "\|file:///usr/lib/gcc/x86_64-linux-gnu/8/include-fixed\|",
+    "\|file:///usr/include\|",
+]
+path = "\|file:///home/masuni/rps-cpp/\|"
 includeDirs = [path]
-#os.system("cd ./project && java -jar rascal.jar Main -srcPath {} -names {} -stdLib {} -includeDir {}".format(path,' '.join(names),' '.join(stdLib),' '.join(includeDirs)))
+# os.system("cd ./project && java -jar rascal.jar Main -srcPath {} -names {} -stdLib {} -includeDir {}".format(path,' '.join(names),' '.join(stdLib),' '.join(includeDirs)))
 print("trying to clean the file")
 utils.cleaner(path)
+with open(os.path.join(utils.get_cleanPath(path), "m3_clean.json"), "r") as clean:
+    parsed = json.load(clean)
+    c = Cpp(utils.get_cleanPath(path),parsed)
+    c.export()
+    print("exported!")
--- parsers/cpp.py
+++ parsers/cpp.py
@@ -1,10 +1,17 @@
 import re
+import os
+import json
+
+
 class Cpp:
     primitives = ["int", "float", "void", "char", "string", "boolean"]
     viz = {"elements": {"nodes": [], "edges": []}}
+    parsed = None
+    path = None
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, path, parsed) -> None:
+        self.path = path
+        self.parsed = parsed
 
     def add_edges(self, kind, content, other=None):
         match kind:
@@ -91,7 +98,9 @@ class Cpp:
                         }
                     }
                 )
-                edge_id = edge_id + hash(other[content[1]["functionName"]]["location"]["file"])
+                edge_id = edge_id + hash(
+                    other[content[1]["functionName"]]["location"]["file"]
+                )
                 self.viz["elements"]["edges"].append(
                     {
                         "data": {
@@ -134,3 +143,113 @@ class Cpp:
                             }
                         }
                     )
+
+    def add_nodes(self, kind, content, issues=None):
+        match kind:
+            case "file":
+                self.viz["elements"]["nodes"].append(
+                    {
+                        "data": {
+                            "id": content,
+                            "properties": {"simpleName": content, "kind": kind},
+                            "labels": ["Container"],
+                        }
+                    }
+                )
+            case "function":
+                vulnerabilities = []
+                self.viz["elements"]["nodes"].append(
+                    {
+                        "data": {
+                            "id": content[0],
+                            "properties": {
+                                "simpleName": content[1]["functionName"],
+                                "kind": kind,
+                                "vulnerabilities": vulnerabilities,
+                                "location": content[1]["location"],
+                            },
+                            "labels": ["Operation"],
+                        }
+                    }
+                )
+
+    def get_files(self):
+        data = self.parsed["provides"]
+        files = []
+        for f in data:
+            files.append(f[0])
+        files = list(dict.fromkeys(files))
+        for f in files:
+            files[files.index(f)] = re.sub("\/.+\/", "", f)
+        return files
+
+    def get_actual_location(self, function):
+        data = self.parsed["functionDefinitions"]
+        location = {}
+        for element in data:
+            if re.match("cpp\+function:", element[0]) and function in element[0]:
+                location["file"], location["position"] = re.split("\(", element[1])
+                location["file"] = re.sub("\|file:.+/", "", location["file"])[:-1]
+                location["position"] = "(" + location["position"]
+                break
+        return location
+
+    def get_functions(self):
+        functions = {}
+        arguments = []
+        data = self.parsed["declaredType"]
+        for element in data:
+            if re.match("cpp\+function:", element[0]):
+                parameter_types = []
+                function = dict()
+                function["functionName"] = re.split(
+                    "\(", re.sub("cpp\+function:.+\/", "", element[0])
+                )[0]
+                function["location"] = self.get_actual_location(
+                    function["functionName"]
+                )
+                returnField = self.get_type_field(element[1]["returnType"])
+                function["returnType"] = self.get_type(
+                    element, "returnType", returnField
+                )
+                if element[1]["parameterTypes"]:
+                    pass
+                functions[function["functionName"]] = function
+        return functions
+
+    def get_type_field(self, element):
+        if "baseType" in element.keys():
+            return "baseType"
+        if "decl" in element.keys():
+            return "decl"
+        if "type" in element.keys():
+            return "type"
+
+    def get_type(self, element, field1, field2):
+        if field2 == "decl":
+            return re.sub("cpp\+class:\/+,", "", element[1][field1][field2])
+        if field2 == "type":
+            if "decl" in element[1][field1][field2].keys():
+                if (
+                    element[1][field1][field2]["decl"]
+                    == "cpp+classTemplate:///std/__cxx11/basic_string"
+                ):
+                    return "string"
+                else:
+                    return re.sub(
+                        "cpp\+class:\/+",
+                        "",
+                        element[1][field1][field2]["decl"],
+                    )
+        if field2 == "baseType":
+            return element[1][field1][field2]
+
+    def export(self):
+        functions = self.get_functions()
+        for func in functions.items():
+            self.add_nodes("function", func, None)
+            self.add_edges("contains", func)
+        for file in self.get_files():
+            self.add_nodes("file", file)
+        with open(os.path.join(self.path, "converted.json"), "w") as f:
+            f.write(json.dumps(self.viz))
