aba0e76761586bac8410523a64812de4f361c878
Matteo Asuni <asunimatteo@gmail.com>
2024-02-04 16:04:32 +0100

    cleanup

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from utils import utils
 from parsers.cpp import Cpp
 
 cpp, names, stdLib, includeDirs, path = utils.parse_settings()
-os.system("cd ./project && java -jar rascal.jar Main -cpp {} -srcPath {} -names {} -stdLib {} -includeDir {}".format(cpp,path,' '.join(names),' '.join(stdLib),' '.join(includeDirs)))
+#os.system("cd ./project && java -jar rascal.jar Main -cpp {} -srcPath {} -names {} -stdLib {} -includeDir {}".format(cpp,path,' '.join(names),' '.join(stdLib),' '.join(includeDirs)))
 print("trying to clean the file\n")
 utils.cleaner(path)
 with open(os.path.join(utils.get_cleanPath(path), "m3_clean.json"), "r") as clean:
