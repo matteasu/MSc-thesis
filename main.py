6993ec1f38847c9f5fbf42ede89aefe84c4026ed
Matteo Asuni <asunimatteo@gmail.com>
2024-02-28 22:56:54 +0100

    Model extraction

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from parsers import *
 from extractor import infer_extractor, mate_poi_extractor
 
 cpp, names, stdLib, includeDirs, path = utils.parse_settings()
-"""os.system(
+os.system(
     "cd ./project && java -jar rascal.jar Main -cpp {} -srcPath {} -names {} -stdLib {} -includeDir {}".format(
         cpp, path, " ".join(names), " ".join(stdLib), " ".join(includeDirs)
     )
-)"""
+)
 print("trying to clean the file\n")
 utils.cleaner(path)
 with open(os.path.join(utils.get_cleanPath(path), "m3_clean.json"), "r") as clean:
@@ -22,7 +22,7 @@ with open(os.path.join(utils.get_cleanPath(path), "m3_clean.json"), "r") as clea
         kind = Cpp
     else:
         kind = C
-    converter = kind(utils.get_cleanPath(path), parsed, infer_extractor(utils.get_cleanPath(path)))
+    converter = kind(utils.get_cleanPath(path), parsed, None)
 
     converter.export()
     print("exported!")
